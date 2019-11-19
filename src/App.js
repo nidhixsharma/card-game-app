1faa6e8a78c82b9fdb450ced35b6421f0fe73c0d
nidhixsharma <nidhi.x.sharma@gmail.com>
2019-11-19 12:34:19 -0800

    Initial Working app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+
+import Result from './components/RandomNumber/Result/Result'
+
 import './App.css';
 
-function App() {
+class App extends Component {
+  state={
+    num1:"",
+    num2:"",
+    sum:"",
+    message:"",
+    answer:"",
+    correct:true
+  }
+
+  componentDidMount(){
+    let firstNumber = Math.floor(Math.random() * 10)
+    let secondNumber = Math.floor(Math.random() * 10)
+    this.setState({
+      num1 : firstNumber,
+      num2 : secondNumber
+    })
+  }
+
+
+    onChangeHandler=(event)=>{
+      this.setState({
+        [event.target.name] : event.target.value
+      })     
+     
+    }
+
+    clickHandler=(a,b,value)=>{    
+      let c= a+b 
+      if(value==c){
+        this.setState({
+          message:"Superb! you got it."
+        })
+      }
+      else{
+        this.setState({
+          message:"OOps!This is inCorrect.",
+          answer: c,
+          correct:false
+        })
+      }
+    }
+
+    refreshPage=()=>{
+      window.location.reload(false);
+    }
+
+  render(){
+    let incorrectMessage =(<h3>{this.state.message} <br/> The correct answer is {this.state.answer}.</h3>)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <h2>React Card App</h2>
+       <div className="flexContainer">
+       {/* <RandomNumber /> */}
+       {this.state.num1}
+       <span > + </span>
+       {this.state.num2}
+       {/* <RandomNumber /> */}
+       <span > = </span>
+       <Result change={this.onChangeHandler}
+               click={()=>{this.clickHandler(this.state.num1,this.state.num2,this.state.sum)}}
+               />       
+       </div>    
+        {this.state.correct ?  <h3>{this.state.message} </h3>: <h3>{incorrectMessage} </h3>}
+      
+      <button  onClick={this.refreshPage}>NEXT</button> 
+           </div>
   );
+}
 }
 
 export default App;
---
+++ src/components/RandomNumber/Result/Result.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './Result.css'
+
+const Result =(props)=>{
+   return( 
+   <div>
+      <div >
+        <input className="inputBox" type= "text" value={props.result} placeholder="" name="sum"  onChange={props.change}/>
+        </div>
+        <div>
+           <button  onClick={props.click}>SUBMIT</button>
+         
+        </div>
+        </div>
+   
+   )
+}
+
+export default Result;
